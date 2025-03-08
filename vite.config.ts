5c9d195d75300a743d1d24211b9579219be2c65c
Gabriel Vitali <vitali.gabriel@gmail.com>
2025-03-08 19:06:04 +0100

    #2 Removing unnecessary `rollupOptions` value

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
 	base: '/birth-chart/',
 	plugins: [vue()],
-	build: {
-		rollupOptions: {
-		  external: ['luxon'],
-		},
-	  },
 	test: {
 		environment: 'jsdom',
 		setupFiles: ['./tests/vitest.setup.ts'],
